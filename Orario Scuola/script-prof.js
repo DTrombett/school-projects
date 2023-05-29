636c5688dab6a87ad77e754191f071de2f4de4cf
DTrombett <maxtromb.dt@gmail.com>
2023-05-29 20:13:55 +0200

    migliora script e stile pagina

---
+++ Orario Scuola/script-prof.js
@@ -0,0 +1,63 @@
+const CLASS_NUMBER_STEP = 256 / 6;
+const FIRST_CHAR_CODE = "A".charCodeAt(0);
+const CLASS_NAME_STEP = 256 / ("I".charCodeAt(0) - FIRST_CHAR_CODE + 2);
+const CLASS_COURSE_STEP = 256 / 3;
+const orario = await fetch("./orario-prof.json").then(b => b.json());
+const table = document.getElementById("table");
+/** @param {string} hash */
+const scrollTo = hash => {
+	document.querySelector(hash)?.scrollIntoView({
+		behavior: "smooth",
+	});
+	history.pushState(null, "", hash);
+};
+
+if (table instanceof HTMLTableElement)
+	document.getElementById("classList")?.addEventListener("click", event => {
+		if (event.target instanceof HTMLAnchorElement) {
+			scrollTo(/** @type {string} */ (event.target.getAttribute("href")));
+			event.preventDefault();
+			return;
+		}
+		if (event.target instanceof HTMLLIElement) {
+			const { textContent } = event.target;
+
+			if (!textContent) return;
+			/** @type {string[][]} */
+			const array = orario[textContent];
+
+			if (!array)
+				return alert(
+					"L'orario per questo professore non è ancora disponibile!"
+				);
+			if (!table.caption) table.caption = table.createCaption();
+			table.caption.textContent = `Orario ${textContent}`;
+			for (let i = 1; i < table.rows.length; i++) {
+				const hour = array[i - 1];
+
+				table.rows[i].style.display = hour ? "table-row" : "none";
+				for (let j = 1; j < table.rows[i].cells.length; j++) {
+					const className = hour?.[j - 1];
+
+					if (!className) {
+						table.rows[i].cells[j].textContent = null;
+						table.rows[i].cells[j].style.backgroundColor = "";
+						continue;
+					}
+					table.rows[i].cells[j].style.backgroundColor = `rgba(${
+						className === "Ricevimenti"
+							? "255, 0, 0"
+							: `${CLASS_NUMBER_STEP * +className[0]}, ${
+									CLASS_NAME_STEP *
+									(className[1].charCodeAt(0) - FIRST_CHAR_CODE + 1)
+							  }, ${CLASS_COURSE_STEP * (className[3] === "S" ? 2 : 1)}`
+					}, 0.5)`;
+					table.rows[i].cells[j].textContent = className;
+				}
+			}
+			scrollTo(`#${textContent[0]}`);
+			table.style.display = "table";
+		}
+	});
+
+export {};
--- Orario Scuola/script.js
+++ Orario Scuola/script.js
@@ -1,10 +1,13 @@
-// const orarioClassi = await fetch("./orario.json").then((b) => b.json());
+const orarioClassi = await fetch("./orario.json").then(b => b.json());
 const table = document.getElementById("table");
+/** @type {Record<string, number[] | undefined>} */
 const colors = {
 	"Arte": [244, 107, 24],
 	"Educazione Fisica": [165, 136, 71],
 	"Filosofia": [130, 218, 242],
 	"Fisica": [252, 173, 190],
+	"Geografia": [120, 116, 63],
+	"Geostoria": [426, 457, 34],
 	"Informatica": [205, 185, 250],
 	"Inglese": [70, 79, 245],
 	"Italiano": [37, 216, 180],
@@ -14,33 +17,68 @@ const colors = {
 	"Scienze": [124, 240, 86],
 	"Storia": [241, 232, 126],
 };
+/** @param {string} hash */
+const scrollTo = hash => {
+	document.querySelector(hash)?.scrollIntoView({
+		behavior: "smooth",
+	});
+	history.pushState(null, "", hash);
+};
 
 if (table instanceof HTMLTableElement)
 	document.getElementById("classList")?.addEventListener("click", event => {
 		if (event.target instanceof HTMLAnchorElement) {
-			document.querySelector(event.target.getAttribute("href")).scrollIntoView({
-				behavior: "smooth",
-			});
+			scrollTo(/** @type {string} */ (event.target.getAttribute("href")));
 			event.preventDefault();
 			return;
 		}
-		if (!(event.target instanceof HTMLLIElement)) return;
-		const { textContent } = event.target;
+		if (event.target instanceof HTMLLIElement) {
+			const { textContent } = event.target;
 
-		if (!textContent) return;
-		// 		/** @type {string[][] | undefined} */
-		// 		const array = orarioClassi[textContent];
-		//
-		// 		if (!array) return;
-		if (!table.caption) table.caption = table.createCaption();
-		table.caption.textContent = `Orario ${textContent}`;
-		for (let i = 1; i < table.rows.length; i++)
-			for (let j = 1; j < table.rows[i].cells.length; j++) {
-				const subject = Object.keys(colors)[Math.floor(Math.random() * 12)];
+			if (!textContent) return;
+			/** @type {string[][]} */
+			const array = orarioClassi[textContent];
 
-				table.rows[i].cells[j].textContent = subject;
-				table.rows[i].cells[j].style.backgroundColor =
-					colors[subject] && `rgba(${colors[subject].join(", ")}, 0.5)`;
+			if (!array)
+				return alert("L'orario di questa classe non è ancora disponibile!");
+			if (!table.caption) table.caption = table.createCaption();
+			table.caption.textContent = `Orario ${textContent}`;
+			for (let i = 1; i < table.rows.length; i++) {
+				const hour = array[i - 1];
+
+				table.rows[i].style.display = hour ? "table-row" : "none";
+				for (let j = 1; j < table.rows[i].cells.length; j++) {
+					// const subject = Object.keys(colors)[Math.floor(Math.random() * 12)];
+					const subject = hour?.[j - 1];
+
+					if (!subject) {
+						table.rows[i].cells[j].textContent = null;
+						table.rows[i].cells[j].style.backgroundColor = "";
+						continue;
+					}
+					let count = 1;
+					const subjects = subject.split("\n");
+					const color = subjects
+						.reduce((a, s, i) => {
+							const color = colors[s];
+
+							if (!color) return a;
+							count++;
+							if (!a) return color;
+							return a.map((n, i) => n + color[i]);
+						}, colors[subjects[0]])
+						?.map(n => n / count);
+
+					table.rows[i].cells[j].textContent = subject;
+					if (color)
+						table.rows[i].cells[j].style.backgroundColor = `rgba(${color.join(
+							", "
+						)}, 0.5)`;
+				}
 			}
-		table.style.display = "table";
+			scrollTo(`#${textContent[1]}`);
+			table.style.display = "table";
+		}
 	});
+
+export {};
