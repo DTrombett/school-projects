8356fc26860b379de28c0d15b3a72327de637fd6
DTrombett <maxtromb.dt@gmail.com>
2023-09-15 15:19:27 +0200

    trova automaticamente classi prof

--- parseData.js
+++ parseData.js
@@ -3,10 +3,11 @@ const { createWriteStream } = require("node:fs");
 
 const parseData = async () => {
 	const doc = await pdfjs.getDocument(
-		"https://www.iisbafile.edu.it/wp-content/uploads/provvisorio-scientifico-11-settembre.pdf"
+		"https://www.iisbafile.edu.it/wp-content/uploads/provvisorio-scientifico-18sett-docenti-1.pdf"
 	).promise;
 	const stream = createWriteStream("file.json", { flags: "a" });
-	const lines = [];
+	const lines = [],
+		teachers = [];
 
 	for (let i = 1; i <= doc.numPages; i++) {
 		const page = await doc.getPage(i);
@@ -18,14 +19,25 @@ const parseData = async () => {
 				if (el.hasEOL) array.push([]);
 			}
 			return array;
-		}, /** @type {(import("pdfjs-dist/types/src/display/api").TextItem | import("pdfjs-dist/types/src/display/api").TextMarkedContent)[][]} */ ([[]]));
+		}, /** @type {import("pdfjs-dist/types/src/display/api").TextItem[][]} */ ([[]]));
 
 		pageLines.splice(0, 14);
 		pageLines.splice(-1, 1);
 		lines.push(...pageLines);
+		teachers.push(
+			...pageLines
+				.filter(l => l[0].transform[5] < 50)
+				.map(t => [
+					t.map(text => text.str).join(""),
+					pageLines.find(
+						l => l[0].transform[4] === t[0].transform[4] && t[0] !== l[0]
+					),
+				])
+		);
 		page.cleanup(true);
 	}
-	stream.write(JSON.stringify(lines));
+	console.log(teachers, teachers.length);
+	stream.write(JSON.stringify(Object.fromEntries(teachers)));
 	stream.end().close();
 	doc.cleanup();
 };
